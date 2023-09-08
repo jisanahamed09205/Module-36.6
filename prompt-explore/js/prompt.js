37a8c8482c46b61619c1e6ea75c4af0a9fdd32e2
jisanahamed09205 <jisanahamed09205@gmail.com>
2023-09-09 03:38:19 +0600

    ds

---
+++ prompt-explore/js/prompt.js
@@ -0,0 +1,20 @@
+// alert('hello!! Look at me!!')
+
+const alertUser = () =>{
+    alert('Be attentive and serious to invest time for learning')
+
+}
+const seriousStudent = () =>{
+    const serious = confirm('Are you serious on your web source ... so why waiting for enroll now')
+    // console.log(serious);
+    if(serious === true){
+        alert('Welcome to our cource')
+    }
+    else{
+        alert('jaw muri khaw')
+    }
+}
+const getName = () => {
+    const name = prompt('what is your name motherboard')
+    console.log(name);
+}
